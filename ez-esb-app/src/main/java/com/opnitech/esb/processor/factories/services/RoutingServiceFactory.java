8475f11fb8732dd4fe085f1ad3a53a8e9f54e653
Rigre Garciandia Sonora <rigre.sonora@dealertrack.com>
2016-04-17 00:16:33 -0500

    Removing old routing implementation

--- ez-esb-app/src/main/java/com/opnitech/esb/processor/factories/services/RoutingServiceFactory.java
+++ ez-esb-app/src/main/java/com/opnitech/esb/processor/factories/services/RoutingServiceFactory.java
@@ -1,17 +1,13 @@
 package com.opnitech.esb.processor.factories.services;
 
-import java.util.List;
-
 import org.elasticsearch.common.inject.Singleton;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.opnitech.esb.processor.configuration.route.RouteConfiguration;
 import com.opnitech.esb.processor.persistence.elastic.repository.document.DocumentRepository;
 import com.opnitech.esb.processor.persistence.jpa.repository.subscriber.SubscriptionRepository;
 import com.opnitech.esb.processor.services.RoutingService;
 import com.opnitech.esb.processor.services.impl.RoutingServiceImpl;
-import com.opnitech.esb.processor.services.impl.routes.RouteExecuter;
 import com.opnitech.esb.processor.services.impl.routes.connection.RouteConnectionContainer;
 
 /**
@@ -26,11 +22,10 @@ public class RoutingServiceFactory {
 
     @Singleton
     @Bean
-    public RoutingService getRoutingService(List<RouteExecuter<? extends RouteConfiguration>> routeExecuters,
-            SubscriptionRepository subscriptionRepository, DocumentRepository documentRepository,
+    public RoutingService getRoutingService(SubscriptionRepository subscriptionRepository, DocumentRepository documentRepository,
             RouteConnectionContainer routeConnectionContainer) {
 
-        RoutingService routingService = new RoutingServiceImpl(routeExecuters, subscriptionRepository, documentRepository,
+        RoutingService routingService = new RoutingServiceImpl(subscriptionRepository, documentRepository,
                 routeConnectionContainer);
 
         return routingService;
--- ez-esb-app/src/main/java/com/opnitech/esb/processor/services/impl/RoutingServiceImpl.java
+++ ez-esb-app/src/main/java/com/opnitech/esb/processor/services/impl/RoutingServiceImpl.java
@@ -1,43 +1,30 @@
 package com.opnitech.esb.processor.services.impl;
 
-import java.util.List;
-
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.Validate;
-
 import com.opnitech.esb.processor.common.data.ElasticIndexMetadata;
 import com.opnitech.esb.processor.common.exception.ServiceException;
-import com.opnitech.esb.processor.configuration.route.RouteConfiguration;
 import com.opnitech.esb.processor.persistence.elastic.repository.document.DocumentRepository;
 import com.opnitech.esb.processor.persistence.jpa.model.consumer.Subscription;
 import com.opnitech.esb.processor.persistence.jpa.repository.subscriber.SubscriptionRepository;
 import com.opnitech.esb.processor.persistence.rabbit.DocumentOutboundCommand;
 import com.opnitech.esb.processor.services.RoutingService;
-import com.opnitech.esb.processor.services.impl.routes.RouteExecuter;
 import com.opnitech.esb.processor.services.impl.routes.connection.RouteConnection;
 import com.opnitech.esb.processor.services.impl.routes.connection.RouteConnectionContainer;
-import com.opnitech.esb.processor.utils.JSONUtil;
 
 /**
  * @author Rigre Gregorio Garciandia Sonora
  */
 public class RoutingServiceImpl implements RoutingService {
 
-    private final List<RouteExecuter<? extends RouteConfiguration>> routeExecuters;
     private final SubscriptionRepository subscriptionRepository;
     private final DocumentRepository documentRepository;
     private final RouteConnectionContainer routeConnectionContainer;
 
-    public RoutingServiceImpl(List<RouteExecuter<? extends RouteConfiguration>> routeExecuters,
-            SubscriptionRepository subscriptionRepository, DocumentRepository documentRepository,
+    public RoutingServiceImpl(SubscriptionRepository subscriptionRepository, DocumentRepository documentRepository,
             RouteConnectionContainer routeConnectionContainer) {
 
         this.subscriptionRepository = subscriptionRepository;
         this.documentRepository = documentRepository;
         this.routeConnectionContainer = routeConnectionContainer;
-        Validate.isTrue(CollectionUtils.isNotEmpty(routeExecuters));
-
-        this.routeExecuters = routeExecuters;
     }
 
     @Override
@@ -47,7 +34,6 @@ public class RoutingServiceImpl implements RoutingService {
                 .findSubscriptionOwnMatchQuery(documentOutboundCommand.getMatchQueryId());
 
         if (subscription != null) {
-
             String documentAsJSON = this.documentRepository.retrieveDocument(
                     new ElasticIndexMetadata(documentOutboundCommand.getVersion(), documentOutboundCommand.getDocumentType()),
                     documentOutboundCommand.getDocumentMetadata().getElasticDocumentId());
@@ -56,30 +42,5 @@ public class RoutingServiceImpl implements RoutingService {
 
             routeConnection.send(documentAsJSON);
         }
-    }
-
-    public void route(RouteConfiguration routeConfiguration, Object payload) {
-
-        Validate.notNull(payload);
-
-        String payloadAsJSON = payload instanceof String
-                ? payload.toString()
-                : JSONUtil.marshall(payload);
-
-        RouteExecuter<? extends RouteConfiguration> routeExecuter = findRouteExecuter(routeConfiguration);
-        Validate.notNull(routeExecuter);
-
-        routeExecuter.doExecuteRoute(routeConfiguration, payloadAsJSON);
-    }
-
-    private RouteExecuter<? extends RouteConfiguration> findRouteExecuter(RouteConfiguration routeConfiguration) {
-
-        for (RouteExecuter<? extends RouteConfiguration> routeExecuter : this.routeExecuters) {
-            if (routeExecuter.acceptRouteConfiguration(routeConfiguration)) {
-                return routeExecuter;
-            }
-        }
-
-        return null;
     }
 }
